5dc2fe5f76b2b4bcb0d1d00b8df0e8aca32e71fa
MaxonSq <koge.egok@gmail.com>
2020-12-15 14:06:31 +0900

    Add files via upload

--- xyz2grd.sh
+++ xyz2grd.sh
@@ -9,11 +9,11 @@ do
     GRDFile=${OldFile}.grd
     
     echo 1. Make Grid $OldFile to $GRDFile.
-    #cat $OldFile |awk '{print $7, $6, $8*-1}' > $NewFile # lat, lon, depth
-    cat $OldFile |gmt mapproject -Ju+54/1:1 -C -I -F > $NewFile # lat, lon, depth
+    cat $OldFile |awk -F" " '{print $2, $1, $3*-1}' > $NewFile # Caris lat, lon, depth
         REGI=`gmt gmtinfo $NewFile -Ie`
         echo "The area setting is: "$REGI
-        gmt nearneighbor $NewFile -G$GRDFile  $REGI -I10e -S0.02m -V # end of making grid
+        echo $GRDFile
+        gmt nearneighbor $NewFile -G$GRDFile  $REGI -I50e -S0.2m -V # end of making grid
     
     echo 2. Make image of $GRDFile. 
         gmt begin  $OldFile pdf
