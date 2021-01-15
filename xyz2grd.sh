576ee9cb3a63cb3c9a7c9c9487c8b2e0db02c9f6
MaxonSq <koge.egok@gmail.com>
2021-01-15 10:53:46 +0900

    Add files via upload

--- xyz2grd.sh
+++ xyz2grd.sh
@@ -6,21 +6,24 @@ for OldFile in $FileExt;
 do
     echo $OldFile
     Na=`basename ${OldFile} .dat`
-    NewFile=${Na}_wgs84.dat 
-    GRDFile=${Na}.grd
+    NewFile=${Na}_wgs84.dat
+    GridInt=100 #(m)
+
+    GRDFile=${Na}_${GridInt}m.grd
     
     echo 1. Make Grid $OldFile to $GRDFile.
     cat $OldFile |awk -F" " '{print $1, $2, $3*-1}' > $NewFile # Caris lat, lon, depth
         REGI=`gmt gmtinfo $NewFile -Ie`
         echo "The area setting is: "$REGI
         echo $GRDFile
-        gmt nearneighbor $NewFile -G$GRDFile  $REGI -I100e -S0.2m -V # end of making grid
+        gmt nearneighbor $NewFile -G$GRDFile  $REGI -I${GridInt}e -S0.2m -V # end of making grid
     
     echo 2. Make image of $GRDFile. 
-        gmt begin  $OldFile pdf
+        gmt begin $OldFile pdf
             gmt grd2cpt $GRDFile -Chaxby -Z  
             gmt grdimage $GRDFile -I+d -B -Y3
-            gmt colorbar -D5/-1/10/0.5h -Baf
+            gmt basemap -BES+t"${Na}"
+            gmt colorbar -Dx5/-1/10/0.5h -Baf
         gmt end
     rm $NewFile
 done;
